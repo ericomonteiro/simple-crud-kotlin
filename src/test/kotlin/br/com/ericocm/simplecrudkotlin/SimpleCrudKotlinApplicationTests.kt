60cac988ed0d7b9c8934449b36e2b549676c39b4
Érico Monteiro <erico.cintra@gmail.com>
2020-06-03 14:06:51 -0300

    Create project

---
+++ src/test/kotlin/br/com/ericocm/simplecrudkotlin/SimpleCrudKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ericocm.simplecrudkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleCrudKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
